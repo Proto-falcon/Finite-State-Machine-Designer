b8b43fbab55c0a1c9f1878e913a19795254b657a
Rilwan Oluwatobi Adeniji <tobiradeniji@gmail.com>
2024-06-01 19:15:20 +0100

    Enabled interactive server components

--- Finite_State_Machine_Designer/Finite_State_Machine_Designer/Program.cs
+++ Finite_State_Machine_Designer/Finite_State_Machine_Designer/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddCascadingAuthenticationState();
@@ -58,6 +59,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(FSMDesigner).Assembly);
 
